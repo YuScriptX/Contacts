313e7ee4ebb1f8fb51aa8369619c4a693c5dac84
YuScripX <yusuf.tiftik@outlook.com>
2025-11-26 13:28:11 +0100

    feat: add initial contact input logic (name, phone, email)

--- contacts.py
+++ contacts.py
@@ -1,2 +1,8 @@
+while True:
 
+    name = str(input("Enter your name: "))
+    telephone = input("Enter your telephone number: ")
+    email = input("Enter your email: ")
+
+    print(f"\n{name}\n {telephone}\n {email}")
     
